987b1cef027d370717476057c5a9aaac246b8559
neverage84 <neverage84@yahoo.com>
2021-08-09 06:01:12 -0600

    seeded user table

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,49 +8,73 @@
 
 dogs = [
   {
+    user_id: 1,
     name: 'Bowie',
     description: 'Bowie dances when he walks'
   },
   {
+    user_id: 1,
     name: 'Brownie',
     description: 'Brownie only sits pretty'
   },
   {
+    user_id: 1,
     name: 'Jax',
     description: '',
   },
   {
+    user_id: 1,
     name: 'Jiro',
     description: 'Jiro takes a long time to destroy his toys'
   },
   {
+    user_id: 1,
     name: 'Pete',
     description: 'Pete has a best friend named Lua'
   },
   {
+    user_id: 2,
     name: 'Bijou',
     description: 'Bijou is the fluffiest of them all'
   },
     {
+    user_id: 2,
     name: 'Britta',
     description: 'Britta has two different colored eyes'
   },
   {
+    user_id: 2,
     name: 'Noodle',
     description: 'Noodle is an Instagram celebrity'
   },
   {
+    user_id: 2,
     name: 'Stella',
     description: 'Stella loves to dig'
   },
   {
+    user_id: 2,
     name: 'Tonks',
     description: 'Tonks loves to run'
   },
 ]
 
+
+
+user = User.new
+user.email = 'test@example.com'
+user.password = '123456'
+user.password_confirmation = '123456'
+user.save!
+
+user = User.new
+user.email = 'test2@example.com'
+user.password = '123456'
+user.password_confirmation = '123456'
+user.save!
+
 dogs.each do |dog|
-  dog = Dog.find_or_create_by(name: dog[:name], description: dog[:description])
+  dog = Dog.find_or_create_by(user_id: dog[:user_id], name: dog[:name], description: dog[:description])
   directory_name = File.join(Rails.root, 'db', 'seed', "#{dog[:name].downcase}", "*")
 
   Dir.glob(directory_name).each do |filename|
